7acd576b08f1514326ca45e9ff50e45dd75bffed
Sylvain Quoilin <squoilin@ulg.ac.be>
2023-05-08 13:31:44 +0200

    Updating publications

--- scripts/get_publications.sh
+++ scripts/get_publications.sh
@@ -4,7 +4,23 @@
 
 wget -O temp.html "https://orbi.uliege.be/reports?action=1&conjunction_1=AND&field_1=author_authority&value_1_authority=&value_1=u203754&year_start=&year_end=&references=&model=none&format=apa&abstract_length=&sort_1=issued_dt%3Adesc&sort_1=author_sort%3Aasc&sort_1=title_sort%3Aasc&output=html&lang_report=en&title=Publications+and+communications+of+Sylvain+Quoilin" 
 head -n -1 temp.html > temp2.html
-tail -n +2 temp2.html > temp3.html
+tail -n +1 temp2.html > temp3.html
+# Remove the first characters until the "</h1>" tag is detected
+
+# Remove the first characters until the "</h1>" tag is detected
+while IFS= read -r line; do
+  if [[ "$line" == *"</h1>"* ]]; then
+    echo "${line#*<\/h1>}" >> temp3.html
+    break
+  else
+    continue
+  fi
+done < temp2.html
+
+# Append the remaining lines to the temporary file
+while IFS= read -r line; do
+  echo "$line" >> temp3.html
+done < "$filename"
 
 #lynx -dump temp3.html    
 
